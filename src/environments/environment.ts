c5891958cf6084d7fc6b188ef5d759517fe0e9fc
Luis Perez <luis.perezpd@uanl.edu.mx>
2018-04-10 20:40:35 -0500

    Agregando el proyecto a GIT

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAR1os_cM_NYL9trqW2VRTVBfr1luWOzW8",
+    authDomain: "angular-crud-firebase-dd53c.firebaseapp.com",
+    databaseURL: "https://angular-crud-firebase-dd53c.firebaseio.com",
+    projectId: "angular-crud-firebase-dd53c",
+    storageBucket: "angular-crud-firebase-dd53c.appspot.com",
+    messagingSenderId: "467653036759"
+  }
 };
